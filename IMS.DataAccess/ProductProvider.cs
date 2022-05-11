d575bf84cef0ed1d3fc1ed4fd1ab955636ba0eba
DevSonuPatidar <102738088+DevSonuPatidar@users.noreply.github.com>
2022-05-11 17:35:29 +0530

    CURD in Product controller added

--- IMS.DataAccess/ProductProvider.cs
+++ IMS.DataAccess/ProductProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IMS.DataAccess.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace IMS.DataAccess
 {
-    class ProductProvider
+    class ProductProvider : BaseProvider
     {
-    }           
+        public ProductProvider()
+        {
+
+        }
+
+        /*public ProductProvider GetProductById(int Id)
+        {
+            return _db.Products.Find(Id);
+        }*/
+
+        public List<Product> GetAllProduct()
+        {
+            var product = _db.Products.ToList();
+            return product;
+        }
+        
+    }
 }
