0457d848df995466ae681916dbc05866f05e9226
DevSonuPatidar <102738088+DevSonuPatidar@users.noreply.github.com>
2022-05-11 11:49:06 +0530

    Added Css and Bootstrap

--- Confi_IMS/Controllers/ProductController.cs
+++ Confi_IMS/Controllers/ProductController.cs
@@ -13,66 +13,66 @@ namespace Confi_IMS.Controllers
     {
         public ActionResult DisplayProduct()
         {
-           //List<Product> list = db.Products.OrderByDescending(x => x.id).ToList();
+           List<Product> list = db.Products.OrderByDescending(x => x.id).ToList();
             return View();
         }
 
 
-        //[HttpGet]
-        //public ActionResult CreateProduct()
-        //{
-        //    return View();
-        //}
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
 
 
-        //[HttpPost]
-        //public ActionResult CreateProduct(Product pro)
-        //{
-        //    db.Product.Add(pro);
-        //    db.SaveChanges();
-        //    return RedirectToAction("DisplayProduct");
-        //}
+        [HttpPost]
+        public ActionResult CreateProduct(Product pro)
+        {
+            db.Product.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+        }
 
-        //[HttpGet]
-        //public ActionResult UpdateProduct(int id)
-        //{
-        //    Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
-        //    return View(pr);
-        //}
+        [HttpGet]
+        public ActionResult UpdateProduct(int id)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pr);
+        }
 
-        //[HttpPost]
-        //public ActionResult UpdateProduct(int id, Product pro)
-        //{
-        //    Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
-        //    pr.Product_Name = pro.Product_Name;
-        //    pr.Product_Quntity = pro.Product_Quntity;
-        //    db.SaveChanges();
-        //    return RedirectToAction("DisplayProduct");
+        [HttpPost]
+        public ActionResult UpdateProduct(int id, Product pro)
+        {
+            Product pr = db.Products.Where(x => x.id == id).SingleOrDefault();
+            pr.Product_Name = pro.Product_Name;
+            pr.Product_Quntity = pro.Product_Quntity;
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
 
-        //}
-        //[HttpGet]
+        }
+        [HttpGet]
 
-        //public ActionResult ProductDetail(int id)
-        //{
-        //    Product pro = db.Products.Where(x => x.id == id).SingleOrDefault();
-        //    return View(pro);
-        //}
+        public ActionResult ProductDetail(int id)
+        {
+            Product pro = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pro);
+        }
 
 
-        //public ActionResult DeleteProduct(int id)
-        //{
-        //    Product pro = db.Products.Where(x => x.id == id).SingleOrDefault();
-        //    return View(pro);
-        //}
+        public ActionResult DeleteProduct(int id)
+        {
+            Product pro = db.Products.Where(x => x.id == id).SingleOrDefault();
+            return View(pro);
+        }
 
-        //[HttpPost]
-        //public ActionResult DeleteProduct(int id, Product pro)
-        //{
-        //    Product p = db.Products.Where(x => x.id == id).SingleOrDefault();
-        //    db.Products.Remove(p);
-        //    db.SaveChanges();
-        //    return RedirectToAction("DisplayProduct");
-        //}
+        [HttpPost]
+        public ActionResult DeleteProduct(int id, Product pro)
+        {
+            Product p = db.Products.Where(x => x.id == id).SingleOrDefault();
+            db.Products.Remove(p);
+            db.SaveChanges();
+            return RedirectToAction("DisplayProduct");
+        }
 
 
 
